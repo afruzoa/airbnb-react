730961d31f0293bc3de3a892d246edcda969adac
Afruzoa <afruzoa1378@gmail.com>
2025-02-22 17:25:58 +0330

    add header

---
+++ src/components/searchbox/search.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import Where from "./wheresection/where";
+import Who from "./whosection/who"
+
+function Search({ activeTab }: { activeTab: "Stays" | "Experiences" }) {
+  console.log({ activeTab });
+  const [whereSectionIsOpen, setWhereSectionIsOpen] = useState<boolean>(false);
+  const [selectedRegion, setSelectedRegion] = useState<string>("");
+  const handleRegionSelect = (region: string) => {
+    setSelectedRegion(region);
+    setWhereSectionIsOpen(false);
+  };
+  return (
+    <div className="search-box">
+      <div className="search-bar">
+        <div className="where-box" onClick={() => setWhereSectionIsOpen(true)}>
+          <div className="search-item">
+            <p className="where">Where</p>
+            <input
+              type="text"
+              value={selectedRegion || ""}
+              placeholder="Search destinations"
+              readOnly
+            />
+          </div>
+        </div>
+        {whereSectionIsOpen && <Where onRegionSelect={handleRegionSelect}/>}
+        {/* <!-- check in box --> */}
+        <div className="checkin-box search-item">
+          <p className="checkin">Check in</p>
+          <input type="text" name="" id="" placeholder="Add dates" readOnly />
+        </div>
+        {/* <!-- check out box --> */}
+        <div className="checkout-box search-item">
+          <p className="checkout">Check Out</p>
+          <input type="text" name="" id="" placeholder="Add dates" readOnly />
+        </div>
+        {/* <!-- date box for exp --> */}
+        <div className="search-item display-none date-box">
+          <p className="date">Date</p>
+          <input type="text" name="" id="" placeholder="Add dates" readOnly />
+        </div>
+        {/* <!-- guest box --> */}
+        <div>
+          <div className="guests-search search-item">
+            <div className="guests-box">
+              <p className="who">Who</p>
+              <input
+                type="text"
+                name=""
+                id="whoInput"
+                placeholder="Add guests"
+                readOnly
+              />
+            </div>
+            <div className="search-bt">
+              <div className="search-button">
+                <img src="src/assets/icon/search.svg" alt="" className="search-icon" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <Who/>
+      </div>
+    </div>
+  );
+}
+
+export default Search;
---
+++ src/components/searchbox/whosection/who.tsx
@@ -0,0 +1,219 @@
+function Who() {
+    return(
+        <div className="who-menu">
+          {/* <div className="inner-who-menu">
+            <div className="age">
+              <div className="adult-text">
+                <div className="bold-adults bold-who-text">Adults</div>
+                <div className="age-range">Ages 13 or above</div>
+              </div>
+              <div className="right-who-menu">
+                <div className="plus-minus">
+                  <button className="minus-button-adult minus-button">
+                    <svg
+                      viewBox="0 0 32 32"
+                      xmlns="http://www.w3.org/2000/svg"
+                      aria-hidden="true"
+                      role="presentation"
+                      focusable="false"
+                      style="
+                          fill: none;
+                          height: 12px;
+                          width: 12px;
+                          stroke: currentcolor;
+                          stroke-width: 5.33333;
+                          overflow: visible;
+                        "
+                    >
+                      <path d="m2 16h28"></path>
+                    </svg>
+                  </button>
+                  <div className="count" id="adultCount">
+                    2
+                  </div>
+                  <button className="plus-button-adult plus-button">
+                    <svg
+                      viewBox="0 0 32 32"
+                      xmlns="http://www.w3.org/2000/svg"
+                      aria-hidden="true"
+                      role="presentation"
+                      focusable="false"
+                      style="
+                          fill: none;
+                          height: 12px;
+                          width: 12px;
+                          stroke: currentcolor;
+                          stroke-width: 5.33333;
+                          overflow: visible;
+                        "
+                    >
+                      <path d="m2 16h28m-14-14v28"></path>
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="age">
+              <div className="Child-text">
+                <div className="bold-child bold-who-text">Children</div>
+                <div className="age-range">Age 2-12</div>
+              </div>
+              <div className="right-who-menu">
+                <div className="plus-minus">
+                  <button className="minus-button minus-button-child">
+                    <svg
+                      viewBox="0 0 32 32"
+                      xmlns="http://www.w3.org/2000/svg"
+                      aria-hidden="true"
+                      role="presentation"
+                      focusable="false"
+                      style="
+                          fill: none;
+                          height: 12px;
+                          width: 12px;
+                          stroke: currentcolor;
+                          stroke-width: 5.33333;
+                          overflow: visible;
+                        "
+                    >
+                      <path d="m2 16h28"></path>
+                    </svg>
+                  </button>
+                  <div className="count" id="childCount">
+                    0
+                  </div>
+                  <button className="plus-button plus-button-child">
+                    <svg
+                      viewBox="0 0 32 32"
+                      xmlns="http://www.w3.org/2000/svg"
+                      aria-hidden="true"
+                      role="presentation"
+                      focusable="false"
+                      style="
+                          fill: none;
+                          height: 12px;
+                          width: 12px;
+                          stroke: currentcolor;
+                          stroke-width: 5.33333;
+                          overflow: visible;
+                        "
+                    >
+                      <path d="m2 16h28m-14-14v28"></path>
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            <div className="age">
+              <div className="Infants-text">
+                <div className="bold-Infants bold-who-text">Infants</div>
+                <div className="age-range">Under 2</div>
+              </div>
+              <div className="right-who-menu">
+                <div className="plus-minus">
+                  <button className="minus-button minus-button-infant">
+                    <svg
+                      viewBox="0 0 32 32"
+                      xmlns="http://www.w3.org/2000/svg"
+                      aria-hidden="true"
+                      role="presentation"
+                      focusable="false"
+                      style="
+                          fill: none;
+                          height: 12px;
+                          width: 12px;
+                          stroke: currentcolor;
+                          stroke-width: 5.33333;
+                          overflow: visible;
+                        "
+                    >
+                      <path d="m2 16h28"></path>
+                    </svg>
+                  </button>
+                  <div className="count" id="infantCount">
+                    0
+                  </div>
+                  <button className="plus-button plus-button-infant">
+                    <svg
+                      viewBox="0 0 32 32"
+                      xmlns="http://www.w3.org/2000/svg"
+                      aria-hidden="true"
+                      role="presentation"
+                      focusable="false"
+                      style="
+                          fill: none;
+                          height: 12px;
+                          width: 12px;
+                          stroke: currentcolor;
+                          stroke-width: 5.33333;
+                          overflow: visible;
+                        "
+                    >
+                      <path d="m2 16h28m-14-14v28"></path>
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            </div>
+        
+            <div className="pet age">
+              <div className="pet-text">
+                <div className="bold-pet bold-who-text">Pets</div>
+                <div>
+                  <a href="http://" className="pet-q">
+                    Bringing a service animal?
+                  </a>
+                </div>
+              </div>
+              <div className="right-who-menu">
+                <div className="plus-minus">
+                  <button className="minus-button minus-button-pet">
+                    <svg
+                      viewBox="0 0 32 32"
+                      xmlns="http://www.w3.org/2000/svg"
+                      aria-hidden="true"
+                      role="presentation"
+                      focusable="false"
+                      style="
+                          fill: none;
+                          height: 12px;
+                          width: 12px;
+                          stroke: currentcolor;
+                          stroke-width: 5.33333;
+                          overflow: visible;
+                        "
+                    >
+                      <path d="m2 16h28"></path>
+                    </svg>
+                  </button>
+                  <div className="count" id="petCount">
+                    0
+                  </div>
+                  <button className="plus-button plus-button-pet">
+                    <svg
+                      viewBox="0 0 32 32"
+                      xmlns="http://www.w3.org/2000/svg"
+                      aria-hidden="true"
+                      role="presentation"
+                      focusable="false"
+                      style="
+                          fill: none;
+                          height: 12px;
+                          width: 12px;
+                          stroke: currentcolor;
+                          stroke-width: 5.33333;
+                          overflow: visible;
+                        "
+                    >
+                      <path d="m2 16h28m-14-14v28"></path>
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div> */}
+        </div>
+    )
+}
+export default Who;
