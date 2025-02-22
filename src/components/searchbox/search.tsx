788164361fae8bcde0e7f4c6cb754b23f6424cd4
Afruzoa <afruzoa1378@gmail.com>
2025-02-22 20:46:41 +0330

    add who section

--- src/components/searchbox/search.tsx
+++ src/components/searchbox/search.tsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
 import Where from "./wheresection/where";
 import Who from "./whosection/who"
-import Checkin from "./checkin/checkin";
 
 function Search({ activeTab }: { activeTab: "Stays" | "Experiences" }) {
   console.log({ activeTab });
   const [whereSectionIsOpen, setWhereSectionIsOpen] = useState<boolean>(false);
+  const [whoSectionIsOpen, setWhoSectionIsOpen] = useState <boolean>(false);
   const [selectedRegion, setSelectedRegion] = useState<string>("");
-  const[checkinSectionIsOpen,setCheckinSectionIsOpen] = useState <boolean>(false);
   const handleRegionSelect = (region: string) => {
     setSelectedRegion(region);
     setWhereSectionIsOpen(false);
@@ -28,11 +27,10 @@ function Search({ activeTab }: { activeTab: "Stays" | "Experiences" }) {
         </div>
         {whereSectionIsOpen && <Where onRegionSelect={handleRegionSelect}/>}
         {/* <!-- check in box --> */}
-        <div className="checkin-box search-item" onClick={() => setCheckinSectionIsOpen(true)}>
+        <div className="checkin-box search-item">
           <p className="checkin">Check in</p>
           <input type="text" name="" id="" placeholder="Add dates" readOnly />
         </div>
-        {checkinSectionIsOpen && <Checkin/>}
         {/* <!-- check out box --> */}
         <div className="checkout-box search-item">
           <p className="checkout">Check Out</p>
@@ -46,7 +44,7 @@ function Search({ activeTab }: { activeTab: "Stays" | "Experiences" }) {
         {/* <!-- guest box --> */}
         <div>
           <div className="guests-search search-item">
-            <div className="guests-box">
+            <div className="guests-box " onClick={() => setWhoSectionIsOpen(true)}>
               <p className="who">Who</p>
               <input
                 type="text"
@@ -56,14 +54,35 @@ function Search({ activeTab }: { activeTab: "Stays" | "Experiences" }) {
                 readOnly
               />
             </div>
+            {whoSectionIsOpen && <Who/>}
             <div className="search-bt">
               <div className="search-button">
-                <img src="src/assets/icon/search.svg" alt="" className="search-icon" />
+                {/* <svg
+                  className="searchicon"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 32 32"
+                  aria-hidden="true"
+                  role="presentation"
+                  focusable="false"
+                  style="
+                      display: block;
+                      fill: none;
+                      height: 16px;
+                      width: 16px;
+                      stroke: white;
+                      stroke-width: 4;
+                      overflow: visible;
+                    "
+                >
+                  <path
+                    fill="none"
+                    d="M13 24a11 11 0 1 0 0-22 11 11 0 0 0 0 22zm8-3 9 9"
+                  ></path>
+                </svg> */}
               </div>
             </div>
           </div>
         </div>
-        <Who/>
       </div>
     </div>
   );
--- src/components/searchbox/whosection/who.tsx
+++ src/components/searchbox/whosection/who.tsx
@@ -1,219 +1,105 @@
-function Who() {
-    return(
-        <div className="who-menu">
-          {/* <div className="inner-who-menu">
-            <div className="age">
-              <div className="adult-text">
-                <div className="bold-adults bold-who-text">Adults</div>
-                <div className="age-range">Ages 13 or above</div>
-              </div>
-              <div className="right-who-menu">
-                <div className="plus-minus">
-                  <button className="minus-button-adult minus-button">
-                    <svg
-                      viewBox="0 0 32 32"
-                      xmlns="http://www.w3.org/2000/svg"
-                      aria-hidden="true"
-                      role="presentation"
-                      focusable="false"
-                      style="
-                          fill: none;
-                          height: 12px;
-                          width: 12px;
-                          stroke: currentcolor;
-                          stroke-width: 5.33333;
-                          overflow: visible;
-                        "
-                    >
-                      <path d="m2 16h28"></path>
-                    </svg>
-                  </button>
-                  <div className="count" id="adultCount">
-                    2
-                  </div>
-                  <button className="plus-button-adult plus-button">
-                    <svg
-                      viewBox="0 0 32 32"
-                      xmlns="http://www.w3.org/2000/svg"
-                      aria-hidden="true"
-                      role="presentation"
-                      focusable="false"
-                      style="
-                          fill: none;
-                          height: 12px;
-                          width: 12px;
-                          stroke: currentcolor;
-                          stroke-width: 5.33333;
-                          overflow: visible;
-                        "
-                    >
-                      <path d="m2 16h28m-14-14v28"></path>
-                    </svg>
-                  </button>
-                </div>
-              </div>
-            </div>
-            <div className="age">
-              <div className="Child-text">
-                <div className="bold-child bold-who-text">Children</div>
-                <div className="age-range">Age 2-12</div>
-              </div>
-              <div className="right-who-menu">
-                <div className="plus-minus">
-                  <button className="minus-button minus-button-child">
-                    <svg
-                      viewBox="0 0 32 32"
-                      xmlns="http://www.w3.org/2000/svg"
-                      aria-hidden="true"
-                      role="presentation"
-                      focusable="false"
-                      style="
-                          fill: none;
-                          height: 12px;
-                          width: 12px;
-                          stroke: currentcolor;
-                          stroke-width: 5.33333;
-                          overflow: visible;
-                        "
-                    >
-                      <path d="m2 16h28"></path>
-                    </svg>
-                  </button>
-                  <div className="count" id="childCount">
-                    0
-                  </div>
-                  <button className="plus-button plus-button-child">
-                    <svg
-                      viewBox="0 0 32 32"
-                      xmlns="http://www.w3.org/2000/svg"
-                      aria-hidden="true"
-                      role="presentation"
-                      focusable="false"
-                      style="
-                          fill: none;
-                          height: 12px;
-                          width: 12px;
-                          stroke: currentcolor;
-                          stroke-width: 5.33333;
-                          overflow: visible;
-                        "
-                    >
-                      <path d="m2 16h28m-14-14v28"></path>
-                    </svg>
-                  </button>
-                </div>
-              </div>
-            </div>
+import { useState } from "react";
 
-            <div className="age">
-              <div className="Infants-text">
-                <div className="bold-Infants bold-who-text">Infants</div>
-                <div className="age-range">Under 2</div>
-              </div>
-              <div className="right-who-menu">
-                <div className="plus-minus">
-                  <button className="minus-button minus-button-infant">
-                    <svg
-                      viewBox="0 0 32 32"
-                      xmlns="http://www.w3.org/2000/svg"
-                      aria-hidden="true"
-                      role="presentation"
-                      focusable="false"
-                      style="
-                          fill: none;
-                          height: 12px;
-                          width: 12px;
-                          stroke: currentcolor;
-                          stroke-width: 5.33333;
-                          overflow: visible;
-                        "
-                    >
-                      <path d="m2 16h28"></path>
-                    </svg>
-                  </button>
-                  <div className="count" id="infantCount">
-                    0
-                  </div>
-                  <button className="plus-button plus-button-infant">
-                    <svg
-                      viewBox="0 0 32 32"
-                      xmlns="http://www.w3.org/2000/svg"
-                      aria-hidden="true"
-                      role="presentation"
-                      focusable="false"
-                      style="
-                          fill: none;
-                          height: 12px;
-                          width: 12px;
-                          stroke: currentcolor;
-                          stroke-width: 5.33333;
-                          overflow: visible;
-                        "
-                    >
-                      <path d="m2 16h28m-14-14v28"></path>
-                    </svg>
-                  </button>
-                </div>
-              </div>
+function Who() {
+  const [adultCount, setAdultCount] = useState(2);
+  const [childCount, setChildCount] = useState(0);
+  const [infantCount, setInfantCount] = useState(0);
+  const [petCount, setPetCount] = useState(0);
+
+  const handleAdultCountChange = (type: string) => {
+    setAdultCount(prevCount => (type === "increase" ? prevCount + 1 : Math.max(0, prevCount - 1)));
+  };
+
+  const handleChildCountChange = (type: string) => {
+    setChildCount(prevCount => (type === "increase" ? prevCount + 1 : Math.max(0, prevCount - 1)));
+  };
+
+  const handleInfantCountChange = (type: string) => {
+    setInfantCount(prevCount => (type === "increase" ? prevCount + 1 : Math.max(0, prevCount - 1)));
+  };
+
+  const handlePetCountChange = (type: string) => {
+    setPetCount(prevCount => (type === "increase" ? prevCount + 1 : Math.max(0, prevCount - 1)));
+  };
+
+  return (
+    <div className="who-menu">
+      <div className="inner-who-menu">
+        <div className="age">
+          <div className="adult-text">
+            <div className="bold-adults bold-who-text">Adults</div>
+            <div className="age-range">Ages 13 or above</div>
+          </div>
+          <div className="right-who-menu">
+            <div className="plus-minus">
+              <button className="minus-button-adult minus-button" onClick={() => handleAdultCountChange("decrease")}>
+                <img src="/src/assets/icon/minus.png" alt="" />
+              </button>
+              <div className="count">{adultCount}</div>
+              <button className="plus-button-adult plus-button" onClick={() => handleAdultCountChange("increase")}>
+              <img src="/src/assets/icon/plus.png" alt="" />
+              </button>
             </div>
-        
-            <div className="pet age">
-              <div className="pet-text">
-                <div className="bold-pet bold-who-text">Pets</div>
-                <div>
-                  <a href="http://" className="pet-q">
-                    Bringing a service animal?
-                  </a>
-                </div>
-              </div>
-              <div className="right-who-menu">
-                <div className="plus-minus">
-                  <button className="minus-button minus-button-pet">
-                    <svg
-                      viewBox="0 0 32 32"
-                      xmlns="http://www.w3.org/2000/svg"
-                      aria-hidden="true"
-                      role="presentation"
-                      focusable="false"
-                      style="
-                          fill: none;
-                          height: 12px;
-                          width: 12px;
-                          stroke: currentcolor;
-                          stroke-width: 5.33333;
-                          overflow: visible;
-                        "
-                    >
-                      <path d="m2 16h28"></path>
-                    </svg>
-                  </button>
-                  <div className="count" id="petCount">
-                    0
-                  </div>
-                  <button className="plus-button plus-button-pet">
-                    <svg
-                      viewBox="0 0 32 32"
-                      xmlns="http://www.w3.org/2000/svg"
-                      aria-hidden="true"
-                      role="presentation"
-                      focusable="false"
-                      style="
-                          fill: none;
-                          height: 12px;
-                          width: 12px;
-                          stroke: currentcolor;
-                          stroke-width: 5.33333;
-                          overflow: visible;
-                        "
-                    >
-                      <path d="m2 16h28m-14-14v28"></path>
-                    </svg>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div> */}
+          </div>
         </div>
-    )
+
+        <div className="age">
+          <div className="Child-text">
+            <div className="bold-child bold-who-text">Children</div>
+            <div className="age-range">Age 2-12</div>
+          </div>
+          <div className="right-who-menu">
+            <div className="plus-minus">
+              <button className="minus-button minus-button-child" onClick={() => handleChildCountChange("decrease")}>
+              <img src="/src/assets/icon/minus.png" alt="" />
+              </button>
+              <div className="count">{childCount}</div>
+              <button className="plus-button plus-button-child" onClick={() => handleChildCountChange("increase")}>
+              <img src="/src/assets/icon/plus.png" alt="" />
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="age">
+          <div className="Infants-text">
+            <div className="bold-Infants bold-who-text">Infants</div>
+            <div className="age-range">Under 2</div>
+          </div>
+          <div className="right-who-menu">
+            <div className="plus-minus">
+              <button className="minus-button minus-button-infant" onClick={() => handleInfantCountChange("decrease")}>
+              <img src="/src/assets/icon/minus.png" alt="" />
+              </button>
+              <div className="count">{infantCount}</div>
+              <button className="plus-button plus-button-infant" onClick={() => handleInfantCountChange("increase")}>
+              <img src="/src/assets/icon/plus.png" alt="" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="pet age">
+          <div className="pet-text">
+            <div className="bold-pet bold-who-text">Pets</div>
+            <div>
+              <a href="http://" className="pet-q">Bringing a service animal?</a>
+            </div>
+          </div>
+          <div className="right-who-menu">
+            <div className="plus-minus">
+              <button className="minus-button minus-button-pet" onClick={() => handlePetCountChange("decrease")}>
+              <img src="/src/assets/icon/minus.png" alt="" className="minus-button"/>
+              </button>
+              <div className="count">{petCount}</div>
+              <button className="plus-button plus-button-pet" onClick={() => handlePetCountChange("increase")}>
+              <img src="/src/assets/icon/plus.png" alt="" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
+
 export default Who;
