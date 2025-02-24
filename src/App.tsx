b2672ee1cb2c40a35952823d9a93e0ccc677ea40
Afruzoa <afruzoa1378@gmail.com>
2025-02-24 16:16:03 +0330

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import Header from "./components/header";
 import 'react-dates/lib/css/_datepicker.css';
 import "/src/App.css";
+import Footer from "./components/footersection/footer";
+import Inspiration from "./components/footersection/inspiration";
 const App = () => {
   return (
-    <div>
-      <Header />
-    </div>
+ <>  
+<main>
+ <Header />
+ <Inspiration/>
+ </main>
+ <Footer/>
+ </>
   );
 };
 
